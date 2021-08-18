5082b57de1ba35e7109a2fb712ad99a070589480
pmc2u2 <pmc2u2@yahoo.co.uk>
2021-08-18 20:58:27 +0100

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Indecision Appp'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  )
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
